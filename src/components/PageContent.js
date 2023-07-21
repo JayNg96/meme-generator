dd5c7a338ebb4d317cd460d2528185107f0ce5c5
Jian Wei Ng <s10208269@connect.np.edu.sg>
2023-07-21 20:42:41 +0800

    Add onChange event & useState to display text on image

--- src/components/PageContent.js
+++ src/components/PageContent.js
@@ -2,26 +2,54 @@ import { useState } from 'react'
 import { memesData } from './../memesData'
 export default function PageContent() {
 
+    const [meme, setMeme] = useState({
+        topText:"", 
+        bottomText:"", 
+        randomImg:"http://i.imgflip.com/1bij.jpg"
+    })
 
-    const [memeImg, setMemeImg] = useState('')
+    // eslint-disable-next-line
+    const [allMemeImages, setAllMemeImages] = useState(memesData)
 
     function getMemeImage(){
-        let getNewImage = memesData.data.memes[Math.floor(Math.random() * memesData.data.memes.length)].url
-        setMemeImg(prevImg => getNewImage)
+        let getNewImage = allMemeImages.data.memes[Math.floor(Math.random() * memesData.data.memes.length)].url
+        setMeme(prevImg => ({...prevImg, randomImg:getNewImage}))
+    }
+    
+    function handleChange(event){
+        setMeme(prevFormState => ({
+            ...prevFormState,
+            [event.target.name] : event.target.value
+        }))
     }
 
     return (
-        <div className="page-content">
-            <main>
-                <div className="meme-input-form">
-                    <div className="textinput-container">
-                        <input className="toptext-input" type="text" placeholder="Enter top text"/>
-                        <input className="bottomtext-input" type="text" placeholder="Enter bottom text"/>
-                    </div>
-                    <button onClick={getMemeImage} id="getmeme-button" type="submit">Get a new meme image 🖼</button>
+        <main>
+            <div className="meme-input-form">
+                <div className="textinput-container">
+                    <input 
+                        name="topText" 
+                        type="text" 
+                        placeholder="Enter top text"
+                        onChange={handleChange}
+                        value={meme.topText}
+                    />
+
+                    <input 
+                        name="bottomText" 
+                        type="text" 
+                        placeholder="Enter bottom text" 
+                        onChange={handleChange}
+                        value={meme.bottomText}
+                    />
                 </div>
-                <img className="meme-image-container" src={memeImg}/>
-            </main>
-        </div>
+                <button onClick={getMemeImage} id="getmeme-button" type="submit">Get a new meme image 🖼</button>
+            </div>
+            <div className='meme-img'>
+                { meme.randomImg && <img className="meme-image-container" src={meme.randomImg} alt=""/> }
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
+            </div>
+        </main>
     )    
 }
